02ca7800c8fa02493ae2107d12e1532f5dbe4d16
ihoorados <hooradramezani@gmail.com>
2022-05-19 10:10:06 +1000

    add UISearchBar

--- Shopping/Main Feature/Presentation/View/HomeViewController.swift
+++ Shopping/Main Feature/Presentation/View/HomeViewController.swift
@@ -48,11 +48,35 @@ class HomeViewController: UIViewController {
     // MARK: User InterFace Properties
     /* ////////////////////////////////////////////////////////////////////// */
 
-    lazy var searchTextFeild: UISearchBar = {
+    lazy var searchBar: UISearchBar = {
 
-        let textFeild = UISearchBar()
-        return textFeild
+        let searchBar = UISearchBar()
+        return searchBar
     }()
+
+    /* ////////////////////////////////////////////////////////////////////// */
+    // MARK: Private Functions
+    /* ////////////////////////////////////////////////////////////////////// */
+
+    fileprivate func setupUIView(){
+
+        self.view.addSubview(searchBar)
+        self.searchBar.placeholder = "Search"
+    }
+
+    fileprivate func setupUILayout(){
+
+        self.searchBar.anchor(top: self.view.safeAreaLayoutGuide.topAnchor,
+                                    left: self.view.leftAnchor,
+                                    right: self.view.rightAnchor,
+                                    paddingLeft: 12.0,
+                                    paddingRight: 12.0)
+    }
+
+    fileprivate func setupDependency(){
+
+        self.searchBar.delegate = self
+    }
 
 }
 
@@ -66,6 +90,38 @@ extension HomeViewController: HomeUserInterface{
     func start() {
 
         print("✅ Start Home UI")
+
+        self.title = "STDev"
         self.view.backgroundColor = .systemBackground
+        self.setupUIView()
+        self.setupUILayout()
+        self.setupDependency()
+    }
+}
+
+
+extension HomeViewController: UISearchBarDelegate{
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        print(searchText)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+
+        self.searchBar.resignFirstResponder()
+        self.searchBar.text = ""
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(true, animated: true)
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(false, animated: false)
     }
 }
